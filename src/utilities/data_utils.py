f528c30ae09fec278ee9988c207a66c5d95ae811
Shivam Mehta <shivam.mehta007@gmail.com>
2021-04-10 21:39:01 +0200

    minor bug fix

--- src/utilities/data_utils.py
+++ src/utilities/data_utils.py
@@ -31,27 +31,27 @@ class CustomCollate:
     
     
     
-    class CustomDataset(Dataset):
-        def __init__(self, hparams, dataset):
-            """Dataset class for torch
+class CustomDataset(Dataset):
+    def __init__(self, hparams, dataset):
+        """Dataset class for torch
 
-            Args:
-                hparams (argparse.Namespace): hyperparmeters if needed for the dataset
-                dataset (Any): dataset to work on
-            """
-            self.hparams = hparams
-            self.dataset = dataset
-        
-        def __getitem__(self, index):
-            """Return one item of the dataset
+        Args:
+            hparams (argparse.Namespace): hyperparmeters if needed for the dataset
+            dataset (Any): dataset to work on
+        """
+        self.hparams = hparams
+        self.dataset = dataset
+    
+    def __getitem__(self, index):
+        """Return one item of the dataset
 
-            Args:
-                index (int): dataloader will fetch it batchwise, just write logic how to get one element
+        Args:
+            index (int): dataloader will fetch it batchwise, just write logic how to get one element
 
-            Returns:
-                Any: one element from the dataset
-            """
-            return self.dataset[index]
-        
-        def __len__(self):
-            return len(self.dataset)
+        Returns:
+            Any: one element from the dataset
+        """
+        return self.dataset[index]
+    
+    def __len__(self):
+        return len(self.dataset)
